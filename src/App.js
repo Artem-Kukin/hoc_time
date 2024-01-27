12a514fcf37f73992721c8aaad889a7fb31c1c89
Artem <a.ku666@vk.com>
2024-01-27 18:00:10 +0200

    projectInit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import moment from "moment/min/moment-with-locales.js";
+moment.locale("ru");
 
-function App() {
+function DateTime(props) {
+  return <p className="date">Было добавленно {props.date}</p>;
+}
+
+function DateTimePretty(props) {
+  let transformTime = moment(`${props.date}`, "YYYYMMDD HH:mm").fromNow();
+  return <DateTime date={transformTime} />;
+}
+
+function Video(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="video">
+      <iframe
+        src={props.url}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+      <DateTimePretty date={props.date} />
     </div>
   );
 }
 
-export default App;
+function VideoList(props) {
+  return props.list.map((item) => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2024-01-26 17:24:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2023-03-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2022-03-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2021-03-03 12:10:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2020-02-03 23:16:00",
+    },
+    {
+      url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
+      date: "2019-01-03 12:10:00",
+    },
+  ]);
+
+  return <VideoList list={list} />;
+}
